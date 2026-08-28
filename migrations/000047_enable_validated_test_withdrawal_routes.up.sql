fbd67bc6ef6907e29dbe9e750f968a55faae019a
tolu <toluking001@gmail.com>
2026-08-28 13:09:34 +0100

    Enable validated staging withdrawal routes

---
+++ migrations/000047_enable_validated_test_withdrawal_routes.up.sql
@@ -0,0 +1,23 @@
+-- Enable only testnet routes confirmed by the Fireblocks Sandbox asset registry.
+UPDATE network_catalog
+SET status = 'enabled'
+WHERE code IN (
+    'solana_testnet', 'polygon_amoy', 'tron_testnet', 'bnb_testnet',
+    'cardano_testnet', 'bitcoin_cash_testnet', 'cosmos_testnet',
+    'avalanche_fuji', 'dogecoin_testnet', 'bitcoin_test'
+);
+
+UPDATE assets
+SET status = 'enabled'
+WHERE (symbol, network, custody_asset_id) IN (
+    ('SOL', 'solana_testnet', 'SOL_TEST'),
+    ('POL', 'polygon_amoy', 'AMOY_POLYGON_TEST'),
+    ('TRX', 'tron_testnet', 'TRX_TEST'),
+    ('BNB', 'bnb_testnet', 'BNB_TEST'),
+    ('ADA', 'cardano_testnet', 'ADA_TEST'),
+    ('BCH', 'bitcoin_cash_testnet', 'BCH_TEST'),
+    ('ATOM', 'cosmos_testnet', 'ATOM_COS_TEST'),
+    ('AVAX', 'avalanche_fuji', 'AVAXTEST'),
+    ('DOGETEST', 'dogecoin_testnet', 'DOGE_TEST'),
+    ('BTC', 'bitcoin_test', 'BTC_TEST')
+);
